b45703d4cc5212b200002cf8090a1d103b0fe06f
Sulman Baig <sulmanweb@gmail.com>
2017-08-06 23:25:31 +0500

    Added automatic detection of browser language and setting fallback language

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 angular.module('confirmation', ['pascalprecht.translate'])
     .config(['$translateProvider', function ($translateProvider) {
-        $translateProvider.translations('en', {
+        // English Variables
+        $translateProvider.translations('en_US', {
             'APP_NAME': 'Work Chat',
             'THANKS': 'Thank You for Subscribing',
             'CONFIRM_SUCCESS_MESSAGE': 'Your email has been successfully confirmed. Thanks for using our services.',
             'FOOTER': 'Work Chat, Sulman Baig, Pakistan'
         });
-        $translateProvider.preferredLanguage('en');
+        // Reference: https://goo.gl/QKuoWz
+        $translateProvider.fallbackLanguage('en_US');
+        // Reference: https://goo.gl/gyS5QP
+        $translateProvider.determinePreferredLanguage();
+        // Reference: https://goo.gl/IUFYbO
         $translateProvider.useSanitizeValueStrategy(null);
     }])
     .controller('ConfirmController', ['$scope', function ($scope) {
 
+        $scope.init = function () {
+            console.log("made by @sulmanweb");
+        };
+
+        $scope.init();
     }]);
